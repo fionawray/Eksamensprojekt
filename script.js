83e1ca9f2999df0e564b6827f2c63e9f1ccc6a74
Rachel <rachelhd.vo@gmail.com>
2019-12-10 12:03:35 +0100

    færdig med skelleten af footer

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 window.addEventListener("DOMContentLoaded", sidenVises);
 
+function sidenVises() {
+    console.log(sidenVises);
+    hentFooter();
+}
+
+async function hentFooter() {
+    const footer = await fetch("inc/footer.html");
+    const including = await footer.text();
+    console.log(footer);
+    document.querySelector("footer").innerHTML = including;
+}
