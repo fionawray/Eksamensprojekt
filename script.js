151c9014cb170b2f42e3690f967d61eb019aab75
nita0018@stud.kea.dk <nita0018@stud.kea.dk>
2019-12-11 13:15:03 +0100

    json link added to about page

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const aboutUrl = "https://uwei.eu/kea/2_semester/eksamen/kristawerdelin/wordpress/wp-json/wp/v2/pages/7";
 
+const aboutCitationUrl = "https://uwei.eu/kea/2_semester/eksamen/kristawerdelin/wordpress/about-citation/"
+
+const aboutUrl = "https://uwei.eu/kea/2_semester/eksamen/kristawerdelin/wordpress/about-2-pt/"
+
 window.addEventListener("DOMContentLoaded", sidenVises);
 
 function sidenVises() {
     console.log(sidenVises);
     getAbout();
+    getAboutCitation();
     hentFooter();
 }
 
 async function getAbout() {
     const about = await fetch(aboutUrl);
     parsedResult = await about.json();
-    document.querySelector(".aboutJsonHer").innerHTML = parsedResult.content.rendered;
+    document.querySelector(".about").innerHTML = parsedResult.content.rendered;
 }
+
+async function getAboutCitation() {
+    const aboutCitation = await fetch(aboutCitationUrl);
+    parsedResult = await aboutCitation.json();
+    document.querySelector(".aboutCitation").innerHTML = parsedResult.content.rendered;
+}
+
 
 async function hentFooter() {
     const footer = await fetch("inc/footer.html");
